d972c9882f0f62437fb295e849b6118d34f9c261
pineapple <601553812@qq.com>
2022-07-14 08:45:43 +0800

    格式化代码,并将JNI改为伴生对象,进一步优化速度.

--- app/src/main/java/com/pxh/jnicomposeimageshop/JNI.kt
+++ app/src/main/java/com/pxh/jnicomposeimageshop/JNI.kt
@@ -1,10 +1,9 @@
 package com.pxh.jnicomposeimageshop
 
-import android.graphics.Bitmap
-
 class JNI {
     init {
         System.loadLibrary("jnicomposeimageshop")
     }
-    external  fun change(bitmap: IntArray,width:Int,height:Int)
+
+    external fun change(bitmap: IntArray, width: Int, height: Int)
 }
--- app/src/main/java/com/pxh/jnicomposeimageshop/MyViewModel.kt
+++ app/src/main/java/com/pxh/jnicomposeimageshop/MyViewModel.kt
@@ -4,35 +4,28 @@ import android.graphics.Bitmap
 import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 class MyViewModel : ViewModel() {
     val _bitmap: MutableLiveData<Bitmap> = MutableLiveData()
 
+    companion object {
+        val jni = JNI()
+    }
+
     fun changeImageByC() {
         val bitmap = _bitmap.value!!
-        //初始化JNI
-        val jni = JNI()
         //获取尺寸
         val size = bitmap.width * bitmap.height
         //创建像素数组
         val pixels = IntArray(size)
-
         //将图片装入像素数组
         bitmap.getPixels(pixels, 0, bitmap.width, 0, 0, bitmap.width, bitmap.height)
         //将像素数组传入JNI
         Log.e("MyViewModel", "changeImageByC: ")
-
-
-                jni.change(pixels, bitmap.width, bitmap.height)
-
-            _bitmap.value =
-                Bitmap.createBitmap(pixels, bitmap.width, bitmap.height, Bitmap.Config.ARGB_8888)
-            Log.e("MyViewModel", "changeImageByC2: ${Thread.currentThread()}")
-
+        jni.change(pixels, bitmap.width, bitmap.height)
+        _bitmap.value =
+            Bitmap.createBitmap(pixels, bitmap.width, bitmap.height, Bitmap.Config.ARGB_8888)
+        Log.e("MyViewModel", "changeImageByC2: ${Thread.currentThread()}")
 
 
         //利用返回的数组创建bitmap
