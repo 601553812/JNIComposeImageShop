80a868b4f3bdf0557152e9d8c9cc62b311fecb61
pineapple <601553812@qq.com>
2022-07-14 09:52:24 +0800

    发现问题:C语言中申请了一个固定大小的数组,但远超正常需求量(已解决) 修改了C语言中数组的定义方式,将内存生命更改为动态获取,使内存利用率提升,使用malloc定义的数组,需要多少内存就申请多少内存,不存在浪费,使用后使用free释放资源.

--- app/src/main/cpp/jnicomposeimageshop.cpp
+++ app/src/main/cpp/jnicomposeimageshop.cpp
@@ -1,5 +1,6 @@
 #include <jni.h>
 #include <string>
+#include <cstdlib>
 #include <android/log.h>
 #define LOG_TAG "JNI"
 #define logv(...) __android_log_print(ANDROID_LOG_VERBOSE,LOG_TAG,__VA_ARGS__)
@@ -7,17 +8,20 @@
 #define logi(...) __android_log_print(ANDROID_LOG_INFO,LOG_TAG,__VA_ARGS__)
 #define logw(...) __android_log_print(ANDROID_LOG_WARN,LOG_TAG,__VA_ARGS__)
 #define loge(...) __android_log_print(ANDROID_LOG_ERROR,LOG_TAG,__VA_ARGS__)
-#define MAX_SIZE 1920*1080*(2<<4)
-int image[MAX_SIZE];
+int *image;
 extern "C"
 JNIEXPORT void JNICALL
 Java_com_pxh_jnicomposeimageshop_JNI_change(JNIEnv *env, jobject thiz, jintArray bitmap, jint width,
                                             jint height) {
     loge("进入JNI");
-    loge("最大转换图片尺寸为%d",MAX_SIZE);
     int size = width * height;
     loge("获取图片尺寸%d",size);
-    loge("--------------------");
+    //sizeof获取一个int变量的大小,乘size得到整个数组所需内存大小.使用malloc申请.
+    image =  (int *)malloc(sizeof (int)*size);
+    if (image== nullptr){
+    loge("分配内存失败,请重新尝试.");
+    }else{
+    loge("分配内存成功,开始读取数组");
     (*env).GetIntArrayRegion(bitmap,0,size,image);
     loge("获取图片数组成功,宽度为%d,高度为%d",width,height);
     for (int i = 0; i < size; ++i) {
@@ -30,4 +34,8 @@ Java_com_pxh_jnicomposeimageshop_JNI_change(JNIEnv *env, jobject thiz, jintArray
     }
     loge("图片转换完毕,准备输出");
     (*env).SetIntArrayRegion(bitmap,0,size,image);
+    free(image);
+    image = nullptr;
+    loge("释放资源完毕");
+    }
 }
